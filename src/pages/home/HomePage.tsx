562265edab1f3bebde38995598c8576bfceb583b
viet-doan <vietdoan.vqd@gmail.com>
2024-07-22 03:23:49 +1000

    Refactored Home Page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import {
+  Box,
   Tab,
   Tabs,
   TabList,
@@ -23,68 +24,80 @@ const HomePage: React.FC<HomePageProps> = ({ goToPage }) => {
   const [currentPage, setCurrentPage] = useState<string>('Discover');
   // Discover Tab
   const discoverTab = (
-    <div style={{ padding: '15px'}}>
-      <div style={{ marginBottom: '15px' }}>
-        <Carousel>
-          <Pill ariaLabel="a pill" onClick={() => {}} text="Adventure" />
-          <Pill ariaLabel="a pill" onClick={() => {}} text="Birthday" />
-          <Pill ariaLabel="a pill" onClick={() => {}} text="Science" />
-          <Pill ariaLabel="a pill" onClick={() => {}} text="Travel" />
-          <Pill ariaLabel="a pill" onClick={() => {}} text="Language Study" />
-          <Pill ariaLabel="a pill" onClick={() => {}} text="Family" />
-        </Carousel>
-      </div>
-      <div style={{ marginTop: '15px' }}>
-        <Grid alignX="stretch" alignY="stretch" columns={2} spacing="1u">
-          <EmbedCard
-            ariaLabel="Add embed to design"
-            description="Puppyhood"
-            onClick={() => {}}
-            onDragStart={() => {}}
-            thumbnailUrl="https://www.canva.dev/example-assets/images/puppyhood.jpg"
-            title="Heartwarming Chatter: Adorable Conversation with a puppy"
-          />
-          <EmbedCard
-            ariaLabel="Add embed to design"
-            description="Puppyhood"
-            onClick={() => {}}
-            onDragStart={() => {}}
-            thumbnailUrl="https://www.canva.dev/example-assets/images/puppyhood.jpg"
-            title="Heartwarming Chatter: Adorable Conversation with a puppy"
-          />
-          <EmbedCard
-            ariaLabel="Add embed to design"
-            description="Puppyhood"
-            onClick={() => {}}
-            onDragStart={() => {}}
-            thumbnailUrl="https://www.canva.dev/example-assets/images/puppyhood.jpg"
-            title="Heartwarming Chatter: Adorable Conversation with a puppy"
-          />
-          <EmbedCard
-            ariaLabel="Add embed to design"
-            description="Puppyhood"
-            onClick={() => {}}
-            onDragStart={() => {}}
-            thumbnailUrl="https://www.canva.dev/example-assets/images/puppyhood.jpg"
-            title="Heartwarming Chatter: Adorable Conversation with a puppy"
-          />
-        </Grid>
-      </div>
-    </div>
+    <Box 
+      paddingTop='2u'
+      paddingBottom='3u'
+    >
+      <Rows spacing='3u'>
+        <Rows spacing='1u'>
+          <Carousel>
+            <Pill ariaLabel="a pill" onClick={() => {}} text="Adventure" />
+            <Pill ariaLabel="a pill" onClick={() => {}} text="Birthday" />
+            <Pill ariaLabel="a pill" onClick={() => {}} text="Science" />
+            <Pill ariaLabel="a pill" onClick={() => {}} text="Travel" />
+            <Pill ariaLabel="a pill" onClick={() => {}} text="Language Study" />
+            <Pill ariaLabel="a pill" onClick={() => {}} text="Family" />
+          </Carousel>
+        </Rows>
+
+        <Rows spacing='1u'>
+          <Grid alignX="stretch" alignY="stretch" columns={2} spacing="1u">
+            <EmbedCard
+              ariaLabel="Add embed to design"
+              description="Puppyhood"
+              onClick={() => {}}
+              onDragStart={() => {}}
+              thumbnailUrl="https://www.canva.dev/example-assets/images/puppyhood.jpg"
+              title="Heartwarming Chatter: Adorable Conversation with a puppy"
+            />
+            <EmbedCard
+              ariaLabel="Add embed to design"
+              description="Puppyhood"
+              onClick={() => {}}
+              onDragStart={() => {}}
+              thumbnailUrl="https://www.canva.dev/example-assets/images/puppyhood.jpg"
+              title="Heartwarming Chatter: Adorable Conversation with a puppy"
+            />
+            <EmbedCard
+              ariaLabel="Add embed to design"
+              description="Puppyhood"
+              onClick={() => {}}
+              onDragStart={() => {}}
+              thumbnailUrl="https://www.canva.dev/example-assets/images/puppyhood.jpg"
+              title="Heartwarming Chatter: Adorable Conversation with a puppy"
+            />
+            <EmbedCard
+              ariaLabel="Add embed to design"
+              description="Puppyhood"
+              onClick={() => {}}
+              onDragStart={() => {}}
+              thumbnailUrl="https://www.canva.dev/example-assets/images/puppyhood.jpg"
+              title="Heartwarming Chatter: Adorable Conversation with a puppy"
+            />
+          </Grid>
+        </Rows>
+      </Rows>
+    </Box>
   );
   // Generate Tab
   const generateTab = (
-    <div style={{ padding: '15px' }}>
-      <Rows spacing="2u">
-        <Title>Finally, you're here! 😊❤️</Title>
-        <Text>
-          We're excited to offer you a one-stop platform for creating children's
-          audiobooks with script generation, character and scene creation, AI
-          narration, and AI music.
-          <br />
-          Let’s embark on this journey together and make something truly
-          special! 🎨📚✨
-        </Text>
+    <Box 
+      paddingTop='2u'
+      paddingBottom='3u'
+    >
+      <Rows spacing="3u">
+        <Rows spacing="1u">
+          <Title>Finally, you're here! 😊❤️</Title>
+          <Text>
+            We're excited to offer you a one-stop platform for creating children's
+            audiobooks with script generation, character and scene creation, AI
+            narration, and AI music.
+            <br />
+            Let’s embark on this journey together and make something truly
+            special! 🎨📚✨
+          </Text>
+        </Rows>
+
         <Button
           variant="primary"
           stretch={true}
@@ -93,28 +106,39 @@ const HomePage: React.FC<HomePageProps> = ({ goToPage }) => {
           Start your journey here
         </Button>
       </Rows>
-    </div>
+    </Box>
   );
 
   return (
-    <div className="scrollContainer">
+    <Box 
+      paddingTop='1u'
+      paddingEnd='2u'
+      paddingBottom='3u'
+    >
       <Tabs>
         <Rows spacing="1u">
           <TabList>
-            <Tab id="discover" onClick={() => setCurrentPage('Discover')}>
+            <Tab 
+              id="discover"
+              onClick={() => setCurrentPage('Discover')}
+            >
               Discover
             </Tab>
-            <Tab id="generate" onClick={() => setCurrentPage('Generate')}>
+            <Tab 
+              id="generate"
+              onClick={() => setCurrentPage('Generate')}
+            >
               Generate
             </Tab>
           </TabList>
+
           <TabPanels>
             <TabPanel id="discover">{discoverTab}</TabPanel>
             <TabPanel id="generate">{generateTab}</TabPanel>
           </TabPanels>
         </Rows>
       </Tabs>
-    </div>
+    </Box>
   );
 };
 
