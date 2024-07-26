a7ca7992893e57e1c732c0ebc5bd806c386780b3
viet-doan <vietdoan.vqd@gmail.com>
2024-07-26 18:56:31 +1000

    Add in Dimensions Selection for Design

--- src/app.tsx
+++ src/app.tsx
@@ -22,7 +22,7 @@ const App: React.FC = () => {
   const renderPage = () => {
     switch (currentPage) {
       case 'Main':
-        return <HomePage goToPage={setCurrentPage} />;
+        return <DesignDescribe goToPage={setCurrentPage} />;
       case 'ScriptDescribe':
         return <ScriptDescribe goToPage={setCurrentPage} />;
       case 'ScriptGenerate':
