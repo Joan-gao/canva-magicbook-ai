226583f8e8084c5725f778966cbd36b9c7fa4acf
viet-doan <vietdoan.vqd@gmail.com>
2024-08-03 13:05:18 +1000

    Add extra loading pages

---
+++ src/pages/Design/DesignLoading.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import {
+  Rows,
+  Columns,
+  Column,
+  Text,
+  Button,
+  Title,
+  Box,
+  ArrowLeftIcon,
+  ProgressBar,
+} from '@canva/app-ui-kit';
+
+interface DesignLoadingProps {
+  goToPage: (page: string) => void;
+}
+
+const DesignLoading: React.FC<DesignLoadingProps> = ({ goToPage }) => {
+  return (
+    <Box 
+    paddingTop='2u'
+    paddingEnd='2u'
+    paddingBottom='3u'
+  >
+    <Rows spacing='3u'>
+      {/* Page Title / Navigation */}
+      <Columns spacing='1.5u'>
+        <Column width='containedContent'>
+          <div
+            style={{background: 'none', border: 'none', cursor:'pointer'}}
+            onClick={() => goToPage('DesignDescribe')}
+          >
+            <ArrowLeftIcon />
+          </div>
+        </Column>
+
+        <Column width='containedContent'>
+          <Title 
+            tone='primary'
+            size='medium'
+            alignment='start'
+          >
+            Generating Design
+          </Title>
+        </Column>
+      </Columns>
+
+      <ProgressBar
+        size="medium"
+        tone="info"
+        value={10}
+      />
+
+    </Rows>
+  </Box>
+  );
+};
+
+export default DesignLoading;
---
+++ src/pages/script/ScriptLoading.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import {
+  Rows,
+  Columns,
+  Column,
+  Text,
+  Button,
+  Title,
+  Box,
+  ArrowLeftIcon,
+  ProgressBar,
+} from '@canva/app-ui-kit';
+
+interface ScriptLoadingProps {
+  goToPage: (page: string) => void;
+}
+
+const ScriptLoading: React.FC<ScriptLoadingProps> = ({ goToPage }) => {
+  return (
+    <Box 
+    paddingTop='2u'
+    paddingEnd='2u'
+    paddingBottom='3u'
+  >
+    <Rows spacing='3u'>
+      {/* Page Title / Navigation */}
+      <Columns spacing='1.5u'>
+        <Column width='containedContent'>
+          <div
+            style={{background: 'none', border: 'none', cursor:'pointer'}}
+            onClick={() => goToPage('ScriptDescribe')}
+          >
+            <ArrowLeftIcon />
+          </div>
+        </Column>
+
+        <Column width='containedContent'>
+          <Title 
+            tone='primary'
+            size='medium'
+            alignment='start'
+          >
+            Generating Script
+          </Title>
+        </Column>
+      </Columns>
+
+      <ProgressBar
+        size="medium"
+        tone="info"
+        value={10}
+      />
+
+    </Rows>
+  </Box>
+  );
+};
+
+export default ScriptLoading;
